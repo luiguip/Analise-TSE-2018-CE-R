f576e1ecca787f8f82bee2c41555bdbd7a688b5a
luiguip <luisugilhermepdea@gmail.com>
2019-11-06 18:24:59 -0300

    adicionados csvs utf-8

--- graficos.R
+++ graficos.R
@@ -1,6 +1,8 @@
-boxplot_eleito <- ggplot(candidatos_receita_despesa, aes(DS_SIT_TOT_TURNO, receita_total))
-boxplot_eleito + geom_boxplot()
-boxplot_eleito + geom_point()
+boxplot_receita <- ggplot(candidatos_receita_despesa, aes(DS_SIT_TOT_TURNO, receita_total))
+boxplot_receita + geom_boxplot()
+
+boxplot_despesa <- ggplot(candidatos_receita_despesa, aes(DS_SIT_TOT_TURNO, despesa_total))
+boxplot_despesa + geom_boxplot()
 
 dispersao_receita_despesa <- ggplot(candidatos_receita_despesa %>% filter(str_detect(DS_SIT_TOT_TURNO, "ELEITO")) , aes(receita_total, despesa_total, colour = DS_SIT_TOT_TURNO))
 dispersao_receita_despesa + geom_point()
